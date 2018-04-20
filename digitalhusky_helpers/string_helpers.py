c8e9897aa299f630b8644c8314c2c95aa4c33515
playday <onedaywemay@gmail.com>
2018-04-21 00:26:06 +0300

    (UPDATE) python3 migrate

--- digitalhusky_helpers/string_helpers.py
+++ digitalhusky_helpers/string_helpers.py
@@ -2,7 +2,7 @@ import re
 import unicodedata
 
 
-all_chars = (unichr(i) for i in xrange(0x110000))
+all_chars = (chr(i) for i in range(0x110000))
 control_chars = ''.join(
     char for char in all_chars
     if unicodedata.category(char) == 'Cc' and char not in ['\r', '\t', '\n']
@@ -10,8 +10,15 @@ control_chars = ''.join(
 control_char_re = re.compile('[%s]' % re.escape(control_chars))
 
 
-def sanitize_control_chars(raw_string):
+def sanitize_bytes_control_chars(raw_bytes):
     """
-    Clean ASCII -- Non printable Characters
+    Clean bytes object ASCII -- Non printable Characters
+    """
+    return control_char_re.sub('', raw_bytes.decode('utf-8')).encode('utf-8')
+
+
+def sanitize_str_control_chars(raw_string):
+    """
+    Clean str object ASCII -- Non printable Characters
     """
     return control_char_re.sub('', raw_string)
